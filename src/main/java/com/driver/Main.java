c4ca65520e8f88f74791c7e3d63e8ce83e978ee7
ysi <yshouldi@gmail.com>
2023-08-10 13:01:02 +0530

    attempt2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,26 +4,11 @@ public class Main {
     public static void main(String args[])
     {
        product ob=new product();
-       System.out.println(ob.product(5,6));
-        System.out.println(ob.product(5.2,7));
-        System.out.println(ob.product(5,6,7));
+      System.out.println(ob.product(5,6));
+      System.out.println(ob.product(5.2,7));
+       System.out.println(ob.product(5,6,7));
 
     }
-    public static class product
-    {
-        public int product(int x, int y)
-        {
-            return x*y;
-        }
-        public int product(int x, int y, int z)
-        {
-            return x*y*z;
-        }
-        public double product(double x, double y)
-        {
-           return x*y;
-        }
 
-    }
 
 }
